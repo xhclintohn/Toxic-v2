dd0781a7d7bc8df5d2ce51323b969c6335f29273
𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧 <152104512+xhclintohn@users.noreply.github.com>
2025-04-20 18:26:47 +0300

    𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧 [Dev]

--- Cmds/General/ping.js
+++ Cmds/General/ping.js
@@ -1,7 +1,7 @@
 //ping.js
 
 module.exports = async (context) => {
-        const { client, m, dreadedspeed } = context;
+        const { client, m, toxicspeed } = context;
 
 
 await m.reply(`Pong\n${dreadedspeed.toFixed(4)}ms`)
