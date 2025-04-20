99beb2b4a2e2db072d6cddb096779b5a8dbebe0f
𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧 <152104512+xhclintohn@users.noreply.github.com>
2025-04-20 19:44:08 +0300

    𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧 [Dev]

--- utility/botUtil/Ownermiddleware.js
+++ utility/botUtil/Ownermiddleware.js
@@ -4,7 +4,7 @@ const Ownermiddleware = async (context, next) => {
     const { m, Owner } = context;
 
     if (!Owner) {
-        return m.reply("You need owner privileges to execute this command.");
+        return m.reply("𝐘𝐨𝐮 𝐚𝐫𝐞 𝐧𝐨𝐭 𝐦𝐲 𝐨𝐰𝐧𝐞𝐫 𝐬𝐨 𝐅𝐯𝐜𝐤 𝐨𝐟𝐟 𝐨𝐫 𝐈'𝐥𝐥 𝐞𝐱𝐭𝐫𝐚𝐦𝐢𝐧𝐚𝐭𝐞 𝐲𝐨𝐮👺💀.");
     }
 
     await next();
