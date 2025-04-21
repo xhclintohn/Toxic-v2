6244cb5422fa655d35e0c3091da93333049294c8
𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧 <152104512+xhclintohn@users.noreply.github.com>
2025-04-21 03:29:09 +0300

    𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧 [Dev]

--- Cmds/Settings/autoread.js
+++ Cmds/Settings/autoread.js
@@ -7,26 +7,21 @@ module.exports = async (context) => {
         const value = args[0]?.toLowerCase();
 
         const settings = await getSettings();
-        // Set default value to 'off' if not exists
-        if (typeof settings.autoread === 'undefined') {
-            await updateSetting('autoread', 'false');
-            settings.autoread = false;
-        }
 
         if (value === 'on') {
             if (settings.autoread) {
-                return await m.reply('◈━━━━━━━━━━━━━━━━◈\n│❒ ✅ Autoread is already ON.\n◈━━━━━━━━━━━━━━━━◈');
+                return await m.reply('✅ Autoread is already ON.');
             }
             await updateSetting('autoread', 'true');
-            await m.reply('◈━━━━━━━━━━━━━━━━◈\n│❒ ✅ Autoread has been turned ON.\n│❒ The bot will now automatically read messages.\n◈━━━━━━━━━━━━━━━━◈');
+            await m.reply('✅ Autoread has been turned ON. The bot will now automatically read messages.');
         } else if (value === 'off') {
             if (!settings.autoread) {
-                return await m.reply('◈━━━━━━━━━━━━━━━━◈\n│❒ ❌ Autoread is already OFF.\n◈━━━━━━━━━━━━━━━━◈');
+                return await m.reply('❌ Autoread is already OFF.');
             }
             await updateSetting('autoread', 'false');
-            await m.reply('◈━━━━━━━━━━━━━━━━◈\n│❒ ❌ Autoread has been turned OFF.\n◈━━━━━━━━━━━━━━━━◈');
+            await m.reply('❌ Autoread has been turned OFF.');
         } else {
-            await m.reply(`◈━━━━━━━━━━━━━━━━◈\n│❒ 📄 Current autoread setting: ${settings.autoread ? 'ON' : 'OFF'}\n│❒ \n│❒ Use _${settings.prefix}autoread on_ or _${settings.prefix}autoread off_.\n◈━━━━━━━━━━━━━━━━◈`);
+            await m.reply(`📄 Current autoread setting: ${settings.autoread ? 'ON' : 'OFF'}\n\nUse _${settings.prefix}autoread on_ or _${settings.prefix}autoread off_.`);
         }
     });
 };
