24812cad0cd545698118b605db14d05450bd5818
𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧 <152104512+xhclintohn@users.noreply.github.com>
2025-04-21 03:27:24 +0300

    𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧 [Dev]

--- Cmds/Settings/anticall.js
+++ Cmds/Settings/anticall.js
@@ -8,31 +8,21 @@ module.exports = async (context) => {
 
         let settings = await getSettings();
         const prefix = settings.prefix;
-        
-        // Set default value to false if not exists
-        if (typeof settings.anticall === 'undefined') {
-            await updateSetting('anticall', false);
-            settings.anticall = false;
-        }
-        
         let isEnabled = settings.anticall === true;
 
         if (value === 'on' || value === 'off') {
             const action = value === 'on';
 
             if (isEnabled === action) {
-                return await m.reply('◈━━━━━━━━━━━━━━━━◈\n│❒ ✅ Anti-call is already ' + value.toUpperCase() + '.\n◈━━━━━━━━━━━━━━━━◈');
+                return await m.reply(`✅ Anti-call is already ${value.toUpperCase()}.`);
             }
 
             await updateSetting('anticall', action ? true : false);
-            await m.reply('◈━━━━━━━━━━━━━━━━◈\n│❒ ✅ Anti-call has been turned ' + value.toUpperCase() + '.\n◈━━━━━━━━━━━━━━━━◈');
+            await m.reply(`✅ Anti-call has been turned ${value.toUpperCase()}.`);
         } else {
             await m.reply(
-                '◈━━━━━━━━━━━━━━━━◈\n' +
-                `│❒ 📄 Current Anti-call setting: ${isEnabled ? 'ON' : 'OFF'}\n` +
-                '│❒ \n' +
-                `│❒ Use "${prefix}anticall on" or "${prefix}anticall off" to change it._\n` +
-                '◈━━━━━━━━━━━━━━━━◈'
+                `📄 Current Anti-call setting: ${isEnabled ? 'ON' : 'OFF'}\n\n` +
+                `_Use "${prefix}anticall on" or "${prefix}anticall off" to change it._`
             );
         }
     });
