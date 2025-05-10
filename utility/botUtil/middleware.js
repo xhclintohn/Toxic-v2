349b8eab7554619de5bfaa5ac6eeb5c6c4be9b29
𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧 <152104512+xhclintohn@users.noreply.github.com>
2025-05-10 18:42:21 +0300

    𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧

--- utility/botUtil/middleware.js
+++ utility/botUtil/middleware.js
@@ -1,16 +1,21 @@
-
-
 const middleware = async (context, next) => {
     const { m, isBotAdmin, isAdmin } = context;
 
     if (!m.isGroup) {
-        return m.reply("This command is meant for groups");
+        return m.reply(`◈━━━━━━━━━━━━━━━━◈
+│❒ This command isn’t for lone wolves. Try again in a group. 🐺
+◈━━━━━━━━━━━━━━━━◈`);
     }
     if (!isAdmin) {
-        return m.reply("You need admin privileges");
+        return m.reply(`◈━━━━━━━━━━━━━━━━◈
+│❒ You think you’re worthy? 
+│❒ Admin privileges are required—go beg for them. 😤
+◈━━━━━━━━━━━━━━━━◈`);
     }
     if (!isBotAdmin) {
-        return m.reply("I need admin privileges");
+        return m.reply(`◈━━━━━━━━━━━━━━━━◈
+│❒ I need admin rights to obey, unlike you who blindly follows. 🫵 
+◈━━━━━━━━━━━━━━━━◈`);
     }
 
     await next(); // Proceed to the next function (main handler)
