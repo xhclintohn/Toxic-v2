52f3ffa2a48dd65ca87719a74f3fe0c799efc97b
𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧 <152104512+xhclintohn@users.noreply.github.com>
2025-05-10 18:40:27 +0300

    𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧

--- utility/botUtil/Ownermiddleware.js
+++ utility/botUtil/Ownermiddleware.js
@@ -1,10 +1,15 @@
-
-
 const Ownermiddleware = async (context, next) => {
     const { m, Owner } = context;
 
     if (!Owner) {
-        return m.reply("𝐘𝐨𝐮 𝐚𝐫𝐞 𝐧𝐨𝐭 𝐦𝐲 𝐨𝐰𝐧𝐞𝐫 𝐬𝐨 𝐅𝐯𝐜𝐤 𝐨𝐟𝐟 𝐨𝐫 𝐈'𝐥𝐥 𝐞𝐱𝐭𝐫𝐚𝐦𝐢𝐧𝐚𝐭𝐞 𝐲𝐨𝐮👺💀.");
+        return m.reply(`◈━━━━━━━━━━━━━━━━◈
+│❒ You dare attempt to use Owner command? 
+
+│❒ Your mere existence is an insult to my code. 
+
+│❒ Crawl back into the abyss where mediocrity thrives before I personally wipe you from this reality fool. 💀👿
+◈━━━━━━━━━━━━━━━━◈
+> ρσɯҽɾԃ Ⴆყ Tσxιƈ ɱԃȥ `);
     }
 
     await next();
