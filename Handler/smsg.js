3d0036eeb0268ea79282f1e5f5e88588e2807c08
𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧 <152104512+xhclintohn@users.noreply.github.com>
2025-05-17 00:48:16 +0300

    𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧 [Dev]

--- Handler/smsg.js
+++ Handler/smsg.js
@@ -8,7 +8,7 @@ const {
   jidDecode,
   proto,
   getContentType,
-} = require("baileys-elite");
+} = require("@whiskeysockets/baileys");
 const { readFileSync } = require('fs');
 
 const path = require('path');
