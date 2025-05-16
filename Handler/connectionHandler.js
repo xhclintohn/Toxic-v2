402acf5cfda293db232e89a6bf576ba9a66d643f
𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧 <152104512+xhclintohn@users.noreply.github.com>
2025-05-17 00:43:21 +0300

    𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧 [Dev]

--- Handler/connectionHandler.js
+++ Handler/connectionHandler.js
@@ -1,6 +1,6 @@
 const { Boom } = require("@hapi/boom");
 const { DateTime } = require("luxon");
-const { default: toxicConnect, DisconnectReason } = require("baileys-elite");
+const { default: toxicConnect, DisconnectReason } = require("@whiskeysockets/baileys");
 const { getSettings, getSudoUsers, addSudoUser } = require("../Database/config");
 const { commands, totalCommands } = require("../Handler/commandHandler");
 
