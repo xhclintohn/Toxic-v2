873ac87814c34d6b225bd1631c7064312b0374c2
xh_clinton <152104512+xhclintohn@users.noreply.github.com>
2025-06-25 16:10:39 +0300

    𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧 [Dev]

--- clintplugins/Search/lyrics.js
+++ clintplugins/Search/lyrics.js
@@ -37,7 +37,7 @@ module.exports = async (context) => {
 
   try {
     const encodedText = encodeURIComponent(text);
-    const apiUrl = `https://api.giftedtech.web.id/api/search/lyrics?apikey=gifted&query=${encodedText}`;
+    const apiUrl = `https://lyricsapi.fly.dev/api/lyrics?q=${encodedText}`;
     const response = await fetch(apiUrl);
     const data = await response.json();
 
