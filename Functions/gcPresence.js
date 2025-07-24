58f8e47cbc82d734bb6bcf987988c42bf3a53b0c
xh_clinton <152104512+xhclintohn@users.noreply.github.com>
2025-07-24 12:47:35 +0300

    𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧

--- Functions/gcPresence.js
+++ Functions/gcPresence.js
@@ -1,13 +1,17 @@
-const { getGroupSetting } = require("../Database/config");
+const { getGroupSettings } = require("../Database/config");
 
 module.exports = async (client, m) => {
     if (!m.isGroup) return;
 
-    const groupSettings = await getGroupSetting(m.chat);
-        const gcpresence = groupSettings?. gcpresence;
+    const groupSettings = await getGroupSettings(m.chat);
+    const gcpresence = groupSettings?.gcpresence;
     if (gcpresence) {
         let presenceTypes = ["recording", "composing"];
         let selectedPresence = presenceTypes[Math.floor(Math.random() * presenceTypes.length)];
-        await client.sendPresenceUpdate(selectedPresence, m.chat);
+        try {
+            await client.sendPresenceUpdate(selectedPresence, m.chat);
+        } catch (e) {
+            console.log("Error in gcPresence:", e);
+        }
     }
 };
