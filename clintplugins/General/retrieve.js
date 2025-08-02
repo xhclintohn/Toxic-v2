a4f268d78a273d357801eeddd0a2fa2805638812
xh_clinton <152104512+xhclintohn@users.noreply.github.com>
2025-08-02 04:53:40 +0300

    𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧 [Dev]

--- clintplugins/General/retrieve.js
+++ clintplugins/General/retrieve.js
@@ -1,18 +1,26 @@
 module.exports = async (context) => {
     const { client, m } = context;
 
-    if (!m.quoted) {
-        return;
-    }
+    if (!m.quoted) return;
 
     const quotedMessage = m.msg?.contextInfo?.quotedMessage;
 
     try {
-        if (quotedMessage.imageMessage) {
-            let imageUrl = await client.downloadAndSaveMediaMessage(quotedMessage.imageMessage);
-            await client.sendMessage(m.chat, { image: { url: imageUrl } }, { quoted: m });
+        if (quotedMessage?.imageMessage) {
+            const imageUrl = await client.downloadAndSaveMediaMessage(quotedMessage.imageMessage);
+            await client.sendMessage(client.user.id, { 
+                image: { url: imageUrl },
+                caption: "Retrieved by Toxic-MD"
+            });
+        } 
+        else if (quotedMessage?.videoMessage) {
+            const videoUrl = await client.downloadAndSaveMediaMessage(quotedMessage.videoMessage);
+            await client.sendMessage(client.user.id, {
+                video: { url: videoUrl },
+                caption: "Retrieved by Toxic-MD"
+            });
         }
     } catch (error) {
-        console.error("Error in view-once command:", error.message);
+       
     }
 };
