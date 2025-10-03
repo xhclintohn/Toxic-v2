97b9005b84df1958dd8a31cb0b30f29e74074d23
xh_clinton <152104512+xhclintohn@users.noreply.github.com>
2025-10-03 16:21:57 +0300

    𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧

---
+++ settings.js
@@ -0,0 +1,15 @@
+module.exports = {
+    prefix: ".",
+    packname: "Toxic-MD",
+    mode: "private",        // private | public
+    presence: "online",     // online | offline | typing | recording
+    autoview: true,
+    autolike: false,
+    autoread: false,
+    autobio: false,
+    anticall: false,
+    chatbotpm: false,
+    autolikeemoji: "❤️",
+    antilink: false,
+    antidelete: false
+};
