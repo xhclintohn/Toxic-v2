5ad450338736b88980f429d04d1ed84bec24dbe9
xh_clinton <152104512+xhclintohn@users.noreply.github.com>
2025-12-20 13:49:11 +0300

    𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧 [Dev]

--- clintplugins/Heroku/update.js
+++ clintplugins/Heroku/update.js
@@ -96,7 +96,7 @@ module.exports = async (context) => {
                 {
                     interactiveMessage: {
                         body: {
-                            text: `🆕 Update Available, Dumbass\n\nNew version found. You're still using outdated garbage.\n\n📌 *Commit:* ${latestCommit.commit.message}\n👤 *Author:* ${latestCommit.commit.author.name}\n🕒 *Date:* ${new Date(latestCommit.commit.author.date).toLocaleString()}\n\nTo update your worthless bot, tap the button below. Don't ask me how to tap, you monkey. 🐒`
+                            text: `🆕 Update Available, Dumbass\n\nNew version found. You're still using outdated garbage.\n\n📌 *Commit:* ${latestCommit.commit.message}\n👤 *Author:* ${latestCommit.commit.author.name}\n🕒 *Date:* ${new Date(latestCommit.commit.author.date).toLocaleString()}\n\nTo update your worthless bot, tap the button below. if you're unable to tap the buttons type ${prefix}trigger Don't ask me how to tap, you monkey. 🐒😂`
                         },
                         footer: {
                             text: "> Pσɯҽɾҽԃ Ⴆყ Tσxιƈ-ɱԃȥ"
