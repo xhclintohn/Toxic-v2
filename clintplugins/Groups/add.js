dceae66f0bfc11064168e9aad4d6b48bdf1d0f77
xh_clinton <152104512+xhclintohn@users.noreply.github.com>
2025-12-23 13:49:17 +0300

    𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧 [Dev]

--- clintplugins/Groups/add.js
+++ clintplugins/Groups/add.js
@@ -1,7 +1,7 @@
-const middleware = require('../../utility/botUtil/middleware');
+const ownerMiddleware = require('../../utility/botUtil/Ownermiddleware');
 
 module.exports = async (context) => {
-    await middleware(context, async () => {
+    await ownerMiddleware(context, async () => {
         const { client, m, participants, botname, groupMetadata, text, pushname } = context;
 
         const { getBinaryNodeChild, getBinaryNodeChildren } = require('@whiskeysockets/baileys');
@@ -34,37 +34,31 @@ module.exports = async (context) => {
             })),
         });
 
-        
-
-        
-
         const add = getBinaryNodeChild(response, 'add');
         const participant = getBinaryNodeChildren(add, 'participant');
 
         let respon = await client.groupInviteCode(m.chat);
 
-        
+        for (const user of participant.filter((item) => item.attrs.error === 401 || item.attrs.error === 403 || item.attrs.error === 408)) {
+            const jid = user.attrs.jid;
+            const content = getBinaryNodeChild(user, 'add_request');
+            const invite_code = content.attrs.code;
+            const invite_code_exp = content.attrs.expiration;
 
-for (const user of participant.filter((item) => item.attrs.error === 401 || item.attrs.error === 403 || item.attrs.error === 408)) {
-    const jid = user.attrs.jid;
-    const content = getBinaryNodeChild(user, 'add_request');
-    const invite_code = content.attrs.code;
-    const invite_code_exp = content.attrs.expiration;
+            let teza;
+            if (user.attrs.error === 401) {
+                teza = `@${jid.split('@')[0]} has blocked the bot.`;
+            } else if (user.attrs.error === 403) {
+                teza = `@${jid.split('@')[0]} has set privacy settings for group adding.`;
+            } else if (user.attrs.error === 408) {
+                teza = `@${jid.split('@')[0]} recently left the group.`;
+            } 
 
-    let teza;
-    if (user.attrs.error === 401) {
-        teza = `@${jid.split('@')[0]} has blocked the bot.`;
-    } else if (user.attrs.error === 403) {
-        teza = `@${jid.split('@')[0]} has set privacy settings for group adding.`;
-    } else if (user.attrs.error === 408) {
-        teza = `@${jid.split('@')[0]} recently left the group.`;
-    } 
+            await m.reply(teza);
 
-    await m.reply(teza);
+            let links = `${pushname} is trying to add or request you to join the group \( {groupMetadata.subject}:\n\nhttps://chat.whatsapp.com/ \){respon}\n\n${botname} 🤖`;
 
-    let links = `${pushname} is trying to add or request you to join the group ${groupMetadata.subject}:\n\nhttps://chat.whatsapp.com/${respon}\n\n${botname} 🤖`;
-
-    await client.sendMessage(jid, { text: links }, { quoted: m });
-}
+            await client.sendMessage(jid, { text: links }, { quoted: m });
+        }
     });
 };
