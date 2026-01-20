b59d581d57c64d372479becbef087797e4fe03e2
xh_clinton <152104512+xhclintohn@users.noreply.github.com>
2026-01-20 21:32:15 +0300

    Update connectionHandler.js

--- Handler/connectionHandler.js
+++ Handler/connectionHandler.js
@@ -77,7 +77,7 @@ async function connectionHandler(socket, connectionUpdate, reconnect) {
     } catch (error) {}
     if (!hasFollowedNewsletter) {
       try {
-        await socket.newsletterFollow("120363322461279856@newsletter");
+        await socket.newsletterFollow("120363406501994814@newsletter");
         hasFollowedNewsletter = true;
       } catch (error) {}
     }
