a27cbb3dd25e8da3eca9f50698ce7c2fb2759496
xh_clinton <152104512+xhclintohn@users.noreply.github.com>
2026-02-09 19:02:38 +0300

    𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧

--- utils/botUtil/middleware.js
+++ utils/botUtil/middleware.js
@@ -2,20 +2,13 @@ const middleware = async (context, next) => {
     const { m, isBotAdmin, isAdmin } = context;
 
     if (!m.isGroup) {
-        return m.reply(`◈━━━━━━━━━━━━━━━━◈
-│❒ This command isn't for lone wolves. Try again in a group. 🐺
-◈━━━━━━━━━━━━━━━━◈`);
+        return m.reply(`╭───( 𝐓𝐨𝐱𝐢𝐜-𝐌D )───\n───≫ Gʀᴏᴜᴘ Oɴʟʏ ≪───\n々 This command isn't for lone wolves.\n々 Try again in a group, you loner. 🐺\n╭───(  )───`);
     }
     if (!isAdmin) {
-        return m.reply(`◈━━━━━━━━━━━━━━━━◈
-│❒ You think you're worthy? 
-│❒ Admin privileges are required—go beg for them. 😤
-◈━━━━━━━━━━━━━━━━◈`);
+        return m.reply(`╭───( 𝐓𝐨𝐱𝐢𝐜-𝐌D )───\n───≫ Nᴏᴛ Aᴅᴍɪɴ ≪───\n々 You think you're worthy?\n々 Admin privileges are required—\n々 go beg for them, peasant. 😤\n╭───(  )───`);
     }
     if (!isBotAdmin) {
-        return m.reply(`◈━━━━━━━━━━━━━━━━◈
-│❒ I need admin rights to obey, unlike you who blindly follows. 🫵 
-◈━━━━━━━━━━━━━━━━◈`);
+        return m.reply(`╭───( 𝐓𝐨𝐱𝐢𝐜-𝐌D )───\n───≫ Bᴏᴛ Nᴏᴛ Aᴅᴍɪɴ ≪───\n々 I need admin rights to obey,\n々 unlike you who blindly follows. 🫵\n々 Make me admin first, idiot.\n╭───(  )───`);
     }
 
     await next();
