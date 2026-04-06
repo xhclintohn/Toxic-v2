1ef7c0dcc0144b7b50fc8e2445c9adece46c9a9a
xh_clinton <152104512+xhclintohn@users.noreply.github.com>
2026-04-06 12:52:10 +0300

    𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧

--- config/settings.js
+++ config/settings.js
@@ -2,17 +2,9 @@ const session = process.env.SESSION || '';
 const mycode = process.env.CODE || "254";
 const botname = process.env.BOTNAME || 'Toxic-MD';
 const herokuAppName = process.env.HEROKU_APP_NAME || '';
-const database = process.env.DATABASE_URL || '';
 
 function getHerokuApiKey() {
     return process.env.HEROKU_API_KEY || '';
 }
 
-module.exports = {
-    session,
-    mycode,
-    botname,
-    database,
-    herokuAppName,
-    getHerokuApiKey
-};
+module.exports = { session, mycode, botname, herokuAppName, getHerokuApiKey };
