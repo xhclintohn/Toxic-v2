15ada8c9df8b18a632e3adc2e330e96577e4ad8c
xh_clinton <152104512+xhclintohn@users.noreply.github.com>
2026-04-08 13:47:35 +0300

    𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧

--- plugins/Owner/getfunc.js
+++ plugins/Owner/getfunc.js
@@ -6,7 +6,7 @@ const normalizeNumber = (jid) => {
     return jid.split('@')[0].split(':')[0].replace(/\D/g, '') + '@s.whatsapp.net';
 };
 
-const DEVELOPER = normalizeNumber('254735342808');
+const DEVELOPER = normalizeNumber('254114885159');
 const MAX_TEXT_SIZE = 3000;
 const FEATURES_DIR = path.join(__dirname, '..', '..', 'features');
 
