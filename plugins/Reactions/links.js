9f44d5f23836893c0dc3b19b876e095fd63cffc3
xh_clinton <152104512+xhclintohn@users.noreply.github.com>
2026-04-10 19:31:07 +0300

    𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧

--- plugins/Reactions/links.js
+++ plugins/Reactions/links.js
@@ -1,6 +1,6 @@
 module.exports = {
-    slap: '',
-    kiss: '',
-    hug: '',
-    fuck: '',
+    slap: 'https://files.catbox.moe/it6901.webp',
+    kiss: 'https://files.catbox.moe/0vdy7y.webp',
+    hug: 'https://files.catbox.moe/ba4zzh.webp',
+    fuck: 'https://files.catbox.moe/c834fx.webp',
 };
