911e4238aaecbcb7b4c22a34e943ffeb7196ccc9
xh_clinton <152104512+xhclintohn@users.noreply.github.com>
2026-04-11 21:06:17 +0300

    fix: remove external names from key variables

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 module.exports = {
     GROQ_API_KEY: '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',
     GITHUB_TOKEN: process.env.GITHUB_TOKEN || '',
-    GIFTED_API_KEY: '_0u5aff45,_0l1876s8qc',
-    GIFTED_API_KEY_FALLBACK: 'gifted'
+    TOXIC_API_KEY: '_0u5aff45,_0l1876s8qc',
+    TOXIC_API_FALLBACK: 'gifted'
 };
