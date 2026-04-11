062ae98016cb3ba0c9411a286eab98b946521315
xh_clinton <152104512+xhclintohn@users.noreply.github.com>
2026-04-11 19:42:23 +0300

    feat: add GIFTED_API_KEY, GIFTED_API_KEY2, GIFTED_API_KEY_FALLBACK

--- keys.js
+++ keys.js
@@ -1,4 +1,7 @@
 module.exports = {
     GROQ_API_KEY: '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',
-    GITHUB_TOKEN: process.env.GITHUB_TOKEN || ''
+    GITHUB_TOKEN: process.env.GITHUB_TOKEN || '',
+    GIFTED_API_KEY: '_0u5aff45',
+    GIFTED_API_KEY2: '_0l1876s8qc',
+    GIFTED_API_KEY_FALLBACK: 'gifted'
 };
