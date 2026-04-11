a37fe1c279705d4e956a34197a5b0b34b9f2f3fd
xh_clinton <152104512+xhclintohn@users.noreply.github.com>
2026-04-11 21:32:53 +0300

    feat: add hug anime command

---
+++ plugins/Anime/hug.js
@@ -0,0 +1,22 @@
+const { getAnime } = require('../../lib/toxicApi');
+
+module.exports = {
+    name: 'hug',
+    aliases: ['animehug', 'glomp'],
+    description: 'Send a hug anime gif',
+    run: async (context) => {
+        const { client, m } = context;
+        try {
+            await client.sendMessage(m.chat, { react: { text: '⌛', key: m.key } });
+            const url = await getAnime('hug');
+            await client.sendMessage(m.chat, { react: { text: '✅', key: m.key } });
+            await client.sendMessage(m.chat, {
+                image: { url },
+                caption: '╭───(    TOXIC-MD    )───\n├───≫ Hᴜɢ ≪───\n╰──────────────────☉\n> ©𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧'
+            }, { quoted: m });
+        } catch (error) {
+            await client.sendMessage(m.chat, { react: { text: '❌', key: m.key } });
+            await m.reply('╭───(    TOXIC-MD    )───\n├───≫ Eʀʀᴏʀ ≪───\n├ \n├ No hugs available!\n╰──────────────────☉\n> ©𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧');
+        }
+    }
+};
