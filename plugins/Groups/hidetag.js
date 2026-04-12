2e3d8a7bf44feeacc911ab463458f70baf1e547a
xh_clinton <152104512+xhclintohn@users.noreply.github.com>
2026-04-12 04:33:22 +0300

    feat: add reactions + remove slow getSettings in group commands

--- plugins/Groups/hidetag.js
+++ plugins/Groups/hidetag.js
@@ -4,7 +4,9 @@ module.exports = async (context) => {
     await middleware(context, async () => {
         const { client, m, args, participants, text } = context;
 
-client.sendMessage(m.chat, { text : text ? text : 'ᅠᅠᅠᅠ' , mentions: participants.map(a => a.id)}, { quoted: m });
+await client.sendMessage(m.chat, { react: { text: '⌛', key: m.key } });
+await client.sendMessage(m.chat, { text : text ? text : 'ᅠᅠᅠᅠ' , mentions: participants.map(a => a.id)}, { quoted: m });
+await client.sendMessage(m.chat, { react: { text: '✅', key: m.key } });
 
 });
 
